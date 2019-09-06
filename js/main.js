e2637144f646532c091cfc763f0767cc48ccd621
Ericksad01 <erickburn01@hotmail.com>
2019-09-06 11:01:45 -0500

    Compresion de css y video

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ $(document).ready(function() {
     return false;
   });
 });
+
+$(document).ready(function() {
+  $("body").hide();
+  $("body").fadeIn(500);
+});
+
+var video = document.getElementById('video');
+video.src = "../img/main2.mp4";
+video.load();
+console.log("Video is on load");
